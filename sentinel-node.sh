276599340eb400e6a6bdb1edb03f712681ceb7b7
RoomIT <admin@roomit.xyz>
2024-01-05 00:28:02 +0700

    Update sentinel-node.sh

--- sentinel-node.sh
+++ sentinel-node.sh
@@ -105,7 +105,7 @@ function depedency:raspbian(){
          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
          tee /etc/apt/sources.list.d/docker.list > /dev/null
          apt-get update -y
-         apt-get install htop docker-ce docker-ce-cli python3-pip containerd.io docker-buildx-plugin docker-compose-plugin jq ufw lsof acl   telnet unzip -y
+         apt-get install htop acl docker-ce docker-ce-cli python3-pip containerd.io docker-buildx-plugin docker-compose-plugin jq ufw lsof acl   telnet unzip -y
          systemctl start docker
 }
 
