cab7e291cf4df0a74a4296c8f0a16803475fe08e
RoomIT <admin@roomit.xyz>
2024-01-05 00:26:15 +0700

    Update sentinel-node.sh

--- sentinel-node.sh
+++ sentinel-node.sh
@@ -94,18 +94,18 @@ function depedency:ubuntu(){
 
 
 function depedency:raspbian(){
-         sudo apt-get update
-         sudo apt-get install ca-certificates curl gnupg
-         sudo install -m 0755 -d /etc/apt/keyrings
+         apt-get update
+         apt-get install ca-certificates curl gnupg
+         install -m 0755 -d /etc/apt/keyrings
          curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-         sudo chmod a+r /etc/apt/keyrings/docker.gpg
+         chmod a+r /etc/apt/keyrings/docker.gpg
 
          echo \
          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/raspbian \
          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-         sudo apt-get update -y
-         sudo apt-get install htop docker-ce docker-ce-cli python3-pip containerd.io docker-buildx-plugin docker-compose-plugin jq ufw lsof acl telegraf  telnet unzip -y
+         tee /etc/apt/sources.list.d/docker.list > /dev/null
+         apt-get update -y
+         apt-get install htop docker-ce docker-ce-cli python3-pip containerd.io docker-buildx-plugin docker-compose-plugin jq ufw lsof acl   telnet unzip -y
          systemctl start docker
 }
 
